a6a671870ee8026dd2e22ec6fefa7059ba894683
vitorvitorgregorio <vitorvitorgregorio@gmail.com>
2022-03-23 23:40:22 -0300

    implementando dark mode

--- js/darkMode.js
+++ js/darkMode.js
@@ -2,7 +2,11 @@ function darkMode(){
     const element = document.querySelector('body');
     const footer = document.querySelector('footer')
     const nav = document.querySelector('nav')
+    const btn = document.getElementById('btn')
+    const logo = document.getElementById('logo')
     element.classList.toggle('dark')
     footer.classList.toggle('dark2')
     nav.classList.toggle('dark2')
+    btn.classList.toggle('btnDark')
+    logo.classList.toggle('logoDark')
 }
